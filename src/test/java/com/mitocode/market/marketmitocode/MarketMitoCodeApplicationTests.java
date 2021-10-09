0b67e2f28d75a9d7f1d643c8aee30a3b82e64a8c
Edwin Vanegas <edwinvasa@gmail.com>
2021-10-08 21:25:30 -0500

    iniciando el proyecto

---
+++ src/test/java/com/mitocode/market/marketmitocode/MarketMitoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitocode.market.marketmitocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketMitoCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
